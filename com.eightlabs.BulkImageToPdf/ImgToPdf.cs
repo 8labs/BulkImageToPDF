4385a597af07f93cd79f6e710652148511c84f47
Kellen Piffner <k@8labs.com>
2011-04-10 22:27:17 +0000

    Adding initial application with basic Image to PDF functionality. MainWindow constructor has quick sample of how to use.

---
+++ com.eightlabs.BulkImageToPdf/ImgToPdf.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Media.Imaging;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace com.eightlabs.BulkImageToPdf
+{
+    public static class ImgToPdf
+    {
+        public static PdfDocument GetDocumentFromFiles(string[] files)
+        {
+            PdfDocument doc = new PdfDocument();
+            doc.Info.Title = "";
+
+            //TODO set some others here 
+            doc.Info.Elements.SetString("/Producer", "8labs Bulk Image To Pdf converter.");
+            doc.Info.Elements.SetString("/Creator", "8labs Bulk Image To Pdf converter.");
+
+            //TODO any sorting, etc... or should happen prior to this function
+            foreach (string f in files)
+            {
+
+                BitmapSource bmp = GetImageFromFile(f);
+                //TODO handle scaling here for better results/speed than the pdf lib scaling??
+                //TODO convert to monochrome or otherwise compress?
+                AddImagePage(doc, bmp); //add a page of the image
+            }
+
+            return doc;
+        }
+
+        public static BitmapSource GetImageFromFile(string fileName)
+        {
+            BitmapImage bi = new BitmapImage();
+            // BitmapImage.UriSource must be in a BeginInit/EndInit block.
+            bi.BeginInit();
+            bi.UriSource = new Uri(fileName, UriKind.RelativeOrAbsolute);
+            bi.EndInit();
+
+            return bi;
+        }
+
+
+        public static void AddImagePage(PdfDocument doc, BitmapSource img)
+        {
+            PdfPage page = doc.AddPage();
+
+            //TODO we need to figure out landscape handling - auto detect or what...
+            //page.Rotate
+
+            using (XGraphics gfx = XGraphics.FromPdfPage(page))
+            using (XImage ximg = XImage.FromBitmapSource(img))
+            {
+                //draw the image full page onto the document (no margins)
+                gfx.DrawImage(ximg, 0, 0, page.Width, page.Height);
+            }
+        }
+
+    }
+}
---
+++ com.eightlabs.BulkImageToPdf/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using PdfSharp.Pdf;
+
+namespace com.eightlabs.BulkImageToPdf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //string[] files = { "sample.TIF", "sample.JPG" };
+            //using (PdfDocument doc = ImgToPdf.GetDocumentFromFiles(files))
+            //{
+            //    doc.Save("test.pdf");
+            //}
+
+        }
+
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
