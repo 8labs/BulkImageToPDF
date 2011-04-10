07a745f2e3b374005b7dd718f90eac0f337d6576
Kellen Piffner <k@8labs.com>
2011-04-10 22:36:09 +0000

    Fixed assembly info and adding solution.

--- com.eightlabs.BulkImageToPdf/Properties/AssemblyInfo.cs
+++ com.eightlabs.BulkImageToPdf/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("com.eightlabs.BulkImageToPdf")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bulk Image to PDF Converter")]
+[assembly: AssemblyDescription("Converts multiple image files into one or many PDF files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("8labs")]
 [assembly: AssemblyProduct("com.eightlabs.BulkImageToPdf")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © 8labs 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
